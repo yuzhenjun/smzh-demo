5826f9d4f625ecbdb8aafb1a5c158f7febc6bed2
zhenjun.yu <zhenjun.yu@sihuatech.com>
2015-03-01 01:40:33 +0800

    web项目合并

--- common.web.core/src/main/webapp/import/imports.js
+++ common.web.core/src/main/webapp/import/imports.js
@@ -6,6 +6,8 @@ with(document){
 	write('<link rel="stylesheet" href="../css/bootstrap.min.css">');
 	//可选的Bootstrap主题文件（一般不用引入）
 	write('<link rel="stylesheet" href="../css/bootstrap-theme.min.css">');
+	
+	write('<link rel="stylesheet" href="../css/dropdown-submenu.css">');
 	//<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
 	write('<script type="text/javascript" language="javascript" src="../js/jquery-1.11.1.min.js"></script>');
 	//<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
