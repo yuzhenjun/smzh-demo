80900ba03dd8a8fba068138408167ce24bdb862d
zhenjun.yu <zhenjun.yu@sihuatech.com>
2015-02-28 17:14:22 +0800

    bootstrap插件

---
+++ common.web/src/main/webapp/import/imports.js
@@ -0,0 +1,14 @@
+/**
+ * 合并js,css文件
+ */
+with(document){
+	//新 Bootstrap 核心 CSS 文件
+	write('<link rel="stylesheet" href="../css/bootstrap.min.css">');
+	//可选的Bootstrap主题文件（一般不用引入）
+	write('<link rel="stylesheet" href="../css/bootstrap-theme.min.css">');
+	//<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	write('<script type="text/javascript" language="javascript" src="../js/jquery-1.11.1.min.js"></script>');
+	//<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	write('<script type="text/javascript" language="javascript" src="../js/bootstrap.min.js"></script>');
+	
+}
