96e696187fc4c501e1f55284d9f4b64a6527becf
zhenjun.yu <zhenjun.yu@sihuatech.com>
2015-03-12 00:37:18 +0800

    jstree http

---
+++ common.web.core/src/main/webapp/demo/HttpClientTest.java
@@ -0,0 +1,28 @@
+package com.sihuatech.utils;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+
+
+public class HttpClientTest {
+
+	public static void main(String[] args) {
+		CloseableHttpClient client = HttpClients.createDefault();  
+		HttpGet get=new HttpGet("http://www.baidu.com");
+		try {
+			CloseableHttpResponse response=client.execute(get);
+			System.out.println(response.getStatusLine().getStatusCode());
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
