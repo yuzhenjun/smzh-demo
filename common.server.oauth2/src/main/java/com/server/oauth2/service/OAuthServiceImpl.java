8c9c40d2709179e966649d7d8f7c62b9b9cfc47e
zhenjun.yu <zhenjun.yu@sihuatech.com>
2015-03-31 10:27:21 +0800

    实现oauth2

--- common.server.oauth2/src/main/java/com/server/oauth2/service/OAuthServiceImpl.java
+++ common.server.oauth2/src/main/java/com/server/oauth2/service/OAuthServiceImpl.java
@@ -1,7 +1,6 @@
 package com.server.oauth2.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
